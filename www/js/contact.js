8c9e02bf3700aaf4f27937b051229870af88f030
nathanxo <nathan-p@outlook.com>
2019-03-28 23:14:39 +0000

    E-MAIL SORTED MAYBE

--- www/js/contact.js
+++ www/js/contact.js
@@ -12,7 +12,7 @@ $(function () {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
+            var url = "contact.html";
 
             // POST values in the background the the script URL
             $.ajax({
